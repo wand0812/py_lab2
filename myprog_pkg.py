568fb5709ac72dda644b0e329620d9fde7d2f66e
wand0812 <dldhks2@gmail.com>
2020-04-27 23:57:28 +0900

    wow yes

--- myprog_pkg.py
+++ myprog_pkg.py
@@ -4,14 +4,13 @@ import sys
 from my_pkg.bina import*
 from my_pkg.union import*
 
-print("Select menu: 1) conversion 2) union/intersection 3) exit?")
-
-a = int(input())
-
-if a == 1:
-    binary()
-if a == 2:
-    listfunc()
-if a == 3:
-    print("exit the program....")
-    sys.exit()
+while True:
+    print("Select menu: 1) conversion 2) union/intersection 3) exit?")
+    a = int(input())
+    if a == 1:
+        binary()
+    if a == 2:
+        listfunc()
+    if a == 3:
+        print("exit the program....")
+        sys.exit()
